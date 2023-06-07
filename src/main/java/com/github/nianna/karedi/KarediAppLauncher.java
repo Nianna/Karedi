2ca33a7cf18a7958bfcbe538072a7b4b298433b5
Nianna <31940002+Nianna@users.noreply.github.com>
2023-06-07 20:57:34 +0200

    Tech/upgrade java to 17 (#14)
    Upgrade dependencies and java to version 17, configure jpackage plugin

---
+++ src/main/java/com/github/nianna/karedi/KarediAppLauncher.java
@@ -0,0 +1,11 @@
+package com.github.nianna.karedi;
+
+public class KarediAppLauncher  {
+
+	private KarediAppLauncher() {
+	}
+
+	public static void main(String[] args) {
+		KarediApp.main(args);
+	}
+}
