2375a5957f03f3de65cf9fe0382b5edacd9b8218
Nianna <31940002+Nianna@users.noreply.github.com>
2023-06-08 00:12:58 +0200

    Modularize app (#15)
    * Modularize application and configure jpackage plugin
    
    * Extract properties for dependencies versions

--- src/main/java/com/github/nianna/karedi/KarediAppLauncher.java
+++ src/main/java/com/github/nianna/karedi/KarediAppLauncher.java
@@ -1,5 +1,9 @@
 package com.github.nianna.karedi;
 
+/**
+ * Workaround required for fat *.jar distribution.
+ * Using {@link KarediApp} as a main class causes error about missing JavaFx libs on startup.
+ */
 public class KarediAppLauncher  {
 
 	private KarediAppLauncher() {
