6a03dab2cf269b646bd9397dee169b57d6bef59c
Weronika Pecio <p.weronika@gmail.com>
2023-06-08 21:14:22 +0200

    Refactor SongDisassembler and add test

--- src/main/java/com/github/nianna/karedi/context/SongDisassembler.java
+++ src/main/java/com/github/nianna/karedi/context/SongDisassembler.java
@@ -8,60 +8,63 @@ import com.github.nianna.karedi.parser.element.TagElement;
 import com.github.nianna.karedi.parser.element.TrackElement;
 import com.github.nianna.karedi.parser.element.VisitableSongElement;
 import com.github.nianna.karedi.song.Note;
-import com.github.nianna.karedi.song.Song;
 import com.github.nianna.karedi.song.SongLine;
 import com.github.nianna.karedi.song.SongTrack;
 import com.github.nianna.karedi.song.tag.Tag;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class SongDisassembler {
 
-	public List<VisitableSongElement> disassemble(Song song) {
-		return disassemble(song.getTags(), song.getTracks());
-	}
-
 	public List<VisitableSongElement> disassemble(List<Tag> tags, List<SongTrack> tracks) {
-		List<VisitableSongElement> elts = disassembleTags(tags);
-		elts.addAll(disassembleTracks(tracks));
-		elts.add(new EndOfSongElement());
-		return elts;
+		return Stream.of(
+						disassembleTags(tags),
+						disassembleTracks(tracks),
+						endSong()
+				)
+				.flatMap(Function.identity())
+				.collect(Collectors.toList());
 	}
 
-	public List<VisitableSongElement> disassembleTags(List<Tag> tags) {
-		return tags.stream().map(this::disassemble).collect(Collectors.toList());
+	private Stream<VisitableSongElement> disassembleTags(List<Tag> tags) {
+		return tags.stream()
+				.map(this::disassemble);
 	}
 
-	public List<VisitableSongElement> disassembleTracks(List<SongTrack> tracks) {
-		List<VisitableSongElement> elts = new ArrayList<>();
-		int trackNumber = 1;
-		for (SongTrack track : tracks) {
-			if (trackNumber > 1 || tracks.size() > 1) {
-				elts.add(new TrackElement(trackNumber++));
-			}
-			elts.addAll(disassemble(track));
-		}
-		return elts;
+	private Stream<VisitableSongElement> disassembleTracks(List<SongTrack> tracks) {
+		boolean shouldSkipFirstPlayerTag = tracks.size() == 1;
+		return tracks.stream()
+				.flatMap(this::disassemble)
+				.skip(shouldSkipFirstPlayerTag ? 1 : 0);
 	}
 
-	public List<VisitableSongElement> disassembleLines(List<SongLine> lines) {
-		return lines.stream().flatMap(line -> {
-			List<VisitableSongElement> elts = disassemble(line);
-			line.getPrevious().ifPresent(prevLine -> {
-				elts.add(0, new LineBreakElement(line.getLineBreak()));
-			});
-			return elts.stream();
-		}).collect(Collectors.toList());
+	private Stream<VisitableSongElement> disassemble(SongTrack track) {
+		return Stream.concat(
+				Stream.of(new TrackElement(track.getPlayer())),
+				disassembleLines(track.getLines())
+		);
 	}
 
-	public List<VisitableSongElement> disassemble(SongTrack track) {
-		return disassembleLines(track.getLines());
+	private Stream<VisitableSongElement> disassembleLines(List<SongLine> lines) {
+		return lines.stream()
+				.flatMap(this::disassembleLine)
+				.skip(1);
 	}
 
-	public List<VisitableSongElement> disassemble(SongLine line) {
-		return line.getNotes().stream().map(this::disassemble).collect(Collectors.toList());
+	private Stream<VisitableSongElement> disassembleLine(SongLine line) {
+		return Stream.concat(
+				Stream.of(new LineBreakElement(line.getLineBreak())),
+				disassembleNotes(line.getNotes())
+		);
+	}
+
+	private Stream<VisitableSongElement> disassembleNotes(List<Note> notes) {
+		return notes
+				.stream()
+				.map(this::disassemble);
 	}
 
 	public NoteElement disassemble(Note note) {
@@ -69,8 +72,13 @@ public class SongDisassembler {
 		if (note.isFirstInLine()) {
 			lyrics = lyrics.trim();
 		}
-		return new NoteElement(noteElementType(note.getType()), note.getStart(), note.getLength(),
-				note.getTone(), lyrics);
+		return new NoteElement(
+				noteElementType(note.getType()),
+				note.getStart(),
+				note.getLength(),
+				note.getTone(),
+				lyrics
+		);
 	}
 
 	private TagElement disassemble(Tag tag) {
@@ -83,7 +91,12 @@ public class SongDisassembler {
 			case FREESTYLE -> NoteElementType.FREESTYLE;
 			case RAP -> NoteElementType.RAP;
 			case GOLDEN_RAP -> NoteElementType.GOLDEN_RAP;
-			default -> NoteElementType.NORMAL;
+			case NORMAL -> NoteElementType.NORMAL;
 		};
 	}
+
+	private Stream<VisitableSongElement> endSong() {
+		return Stream.of(new EndOfSongElement());
+	}
+
 }
