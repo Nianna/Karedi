088afa35f6e844195fa4a5f22090046558bb4fb5
Nianna <31940002+Nianna@users.noreply.github.com>
2023-09-20 16:26:48 +0200

    Reset midi sequencer on song reload (#36)

--- src/main/java/com/github/nianna/karedi/context/actions/ReloadSongAction.java
+++ src/main/java/com/github/nianna/karedi/context/actions/ReloadSongAction.java
@@ -2,6 +2,7 @@ package com.github.nianna.karedi.context.actions;
 
 
 import com.github.nianna.karedi.KarediApp;
+import com.github.nianna.karedi.action.KarediActions;
 import com.github.nianna.karedi.context.AppContext;
 import javafx.event.ActionEvent;
 
@@ -18,6 +19,7 @@ class ReloadSongAction extends ContextfulKarediAction {
     protected void onAction(ActionEvent event) {
         if (KarediApp.getInstance().saveChangesIfUserWantsTo()) {
             backupTrackAndLine();
+            executeAction(KarediActions.RESET_SEQUENCER);
             txtContext.loadSongFile(txtContext.getActiveFile(), false);
 
             if (activeSongContext.getSong() != null) {
